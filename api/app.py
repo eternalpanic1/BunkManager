f08c7bada75e8c7dbb4d08984784d0970945d961
eternalpanic1 <navaneethkrishnamudumba@gmail.com>
2024-03-29 09:12:47 +0530

    deployment v1.1

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
-file = open('config.json')
+file = open('static\config.json')
 firebaseConfig = json.load(file)
 
 firebase = pyrebase.initialize_app(firebaseConfig)
